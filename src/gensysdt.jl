e47192623ddcc1f2cacd152ebfa215a652676337
gavinecon <85758910+gavinecon@users.noreply.github.com>
2022-11-24 17:48:24 +0000

    Update gensysdt.jl

--- src/gensysdt.jl
+++ src/gensysdt.jl
@@ -1,42 +1,33 @@
 # This code is based on a routine originally copyright Chris Sims.
-# See http://sims.princeton.edu/yftp/gensys/
+# See http://sims.princeton.edu/yftp/gensysdt/
 
 """
 ```
-gensysct(Γ0, Γ1, c, Ψ, Π)
-gensysct(Γ0, Γ1, c, Ψ, Π, div)
-gensysct(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π)
-gensysct(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π, div)
+gensysdt(Γ0, Γ1, c, Ψ, Π)
+gensysdt(Γ0, Γ1, c, Ψ, Π, div)
+gensysdt(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π)
+gensysdt(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π, div)
 ```
 
 Generate state-space solution to canonical-form DSGE model.
 
 System given as
 ```
-Γ0*Dy(t) = Γ1*y(t) + c + Ψ*z(t) + Π*η(t),
+Γ0*y(t) = Γ1*y(t-1) + c + Ψ*z(t) + Π*η(t),
 ```
-with z an exogenous variable process and η being endogenously white noise expectational errors.
+with z an exogenous variable process and η being endogenously
+determined one-step-ahead expectational errors.
 
 Returned system is
 ```
-Dy(t) = G1*y(t) + C + impact * z(t)
+y(t) = G1*y(t-1) + C + impact*z(t) + ywt*inv(I-fmat*inv(L))*fwt*z(t+1)
 ```
-
-
 Returned values are
 ```
-G1, C, impact, qt', a, b, z, eu
+G1, C, impact, fmat, fwt, ywt, gev, eu, loose
 ```
 
-Also returned is the qz decomposition, qt'az' = Γ0, qt'bz' = Γ1, with a and b
-upper triangular and the system ordered so that all zeros on the diagonal of b are in
-the lower right corner, all cases where the real part of bii/aii is greater than or
-equal to div appear in the next block above the zeros, and the remaining bii/aii's
-all have bii/aii<div .  These elements can be used to construct the full backward and
-forward solution.  See the paper \"Solving Linear Rational Expectations Models\",
-http://eco-072399b.princeton.edu/yftp/gensys .  Note that if one simply wants the backwar
-and forward projection of y on eΨlon, ignoring existence and uniqueness questions, the
-projection can be computed by Fourier methods.
+If `z(t)` is i.i.d., the last term drops out.
 
 If `div` is omitted from argument list, a `div`>1 is calculated.
 
@@ -44,24 +35,40 @@ If `div` is omitted from argument list, a `div`>1 is calculated.
 
 * `eu[1]==1` for existence
 * `eu[2]==1` for uniqueness
-* `eu[1]==-1` for existence for white noise η
+* `eu[1]==-1` for existence only with not-s.c. z;
 * `eu==[-2,-2]` for coincident zeros.
+
+### Notes
+
+We constrain Julia to use the complex version of the `schurfact` routine regardless of the
+types of `Γ0` and `Γ1`, to match the behavior of Matlab.  Matlab always uses the complex version
+of the Schur decomposition, even if the inputs are real numbers.
 """
 
+function Ac_mul_B(a,b)
+    balls = a'*b
+    return balls
+end
 
+function A_mul_Bc(a,b)
+    balls = a*b'
+    return balls
+end
+
+
+function gensysdt(Γ0, Γ1, c, Ψ, Π, args...)
+    F = schur!(complex(Γ0), complex(Γ1))
+    gensysdt(F, c, Ψ, Π, args...)
+end
+
+function gensysdt(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π)
+    gensysdt(F, c, Ψ, Π, new_div(F))
+end
 
 const ϵ = sqrt(eps()) * 10
 
-function gensysct(Γ0, Γ1, c, Ψ, Π, args...)
-    F = schur!(complex(Γ0), complex(Γ1))
-    gensysct(F, c, Ψ, Π, args...)
-end
-
-function gensysct(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π)
-    gensysct(F, c, Ψ, Π, new_divct(F))
-end
-
-function gensysct(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π, div)
+# Method that does the real work. Work directly on the decomposition F
+function gensysdt(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π, div)
     eu = [0, 0]
     a, b = F.S, F.T
     n = size(a, 1)
@@ -70,86 +77,112 @@ function gensysct(F::LinearAlgebra.GeneralizedSchur, c, Ψ, Π, div)
         if (abs(a[i, i]) < ϵ) && (abs(b[i, i]) < ϵ)
             info("Coincident zeros.  Indeterminacy and/or nonexistence.")
             eu = [-2, -2]
-            G1 = Array{Float64, 2}() ;  C = Array{Float64, 1}() ; impact = Array{Float64, 2}()
-            a, b, qt, z = FS.S, FS.T, FS.Q, FS.Z
-            return G1, C, impact, qt', a, b, z, eu
+            G1 = Array{Float64, 2}() ;  C = Array{Float64, 1}() ; impact = Array{Float64, 2}() ; fmat = Array{Complex{Float64}, 2}() ; fwt = Array{Complex{Float64}, 2}() ; ywt = Vector{Complex{Float64}}() ; gev = Vector{Complex{Float64}}() ; loose = Array{Float64, 2}()
+            return G1, C, impact, fmat, fwt, ywt, gev, eu, loose
         end
     end
-    movelast = Bool[(real(b[i, i] / a[i, i]) > div) || (abs(a[i, i]) < ϵ) for i in 1:n]
+
+
+    # ass = [0, 1, 1, 0, 0, 0, 0, 0]
+    # ass = Vector{Bool}([1, 0, 0, 1, 1, 1, 1, 1])
+    # ordschur!(F, not(ass))
+    movelast = Bool[abs(b[i, i]) > div * abs(a[i, i]) for i in 1:n]
+    #print(movelast)
     nunstab = sum(movelast)
-    FS = ordschur!(F, .!movelast)
+    FS = ordschur!(F, .!movelast) ## check here
     a, b, qt, z = FS.S, FS.T, FS.Q, FS.Z
 
+
+    gev = hcat(diag(a), diag(b))
     qt1 = qt[:, 1:(n - nunstab)]
     qt2 = qt[:, (n - nunstab + 1):n]
-    a2 = a[(n - nunstab + 1):n, (n - nunstab + 1):n]
-    b2 = b[(n - nunstab + 1):n, (n - nunstab + 1):n]
     etawt = Ac_mul_B(qt2, Π)
-    bigev, ueta, deta, veta = decomposition_svdct!(etawt)
-    zwt = Ac_mul_B(qt2, Ψ)
-    bigev, uz, dz, vz = decomposition_svdct!(zwt)
-    if isempty(bigev)
-        exist = true
+    neta = size(Π, 2)
+
+    # branch below is to handle case of no stable roots, rather than quitting with an error
+    # in that case.
+    if nunstab == 0
+        etawt = zeros(0, neta)
+        ueta = zeros(0, 0)
+        deta = zeros(0, 0)
+        veta = zeros(neta, 0)
+        bigev = 0
     else
-        exist = norm(uz- A_mul_Bc(ueta, ueta) * uz, 2) < ϵ * n
+        bigev, ueta, deta, veta = decomposition_svd!(etawt)
     end
-    if isempty(bigev)
-        existx = true
+
+    eu[1] = length(bigev) >= nunstab
+
+    # Note that existence and uniqueness are not just matters of comparing
+    # numbers of roots and numbers of endogenous errors.  These counts are
+    # reported below because usually they point to the source of the problem.
+
+    # branch below to handle case of no stable roots
+    if nunstab == n
+        etawt1 = zeros(0, neta)
+        bigev = 0
+        ueta1 = zeros(0, 0)
+        veta1 = zeros(neta, 0)
+        deta1 = zeros(0, 0)
     else
-        zwtx0 = b2 \ zwt
-        zwtx = zwtx0
-        M = b2 \ a2
-        M = rmul!(M, 1 / norm(M))
-        for i in 2:nunstab
-            zwtx = hcat(M * zwtx, zwtx0)
-        end
-        zwtx = b2 * zwtx
-        bigev, ux, dx, vx = decomposition_svdct!(zwtx)
-        existx = norm(ux - A_mul_Bc(ueta, ueta) * ux, 2) < ϵ * n
+        etawt1 = Ac_mul_B(qt1, Π)
+        ndeta1 = min(n - nunstab, neta)
+        bigev, ueta1, deta1, veta1 = decomposition_svd!(etawt1)
     end
-    etawt1 = Ac_mul_B(qt1, Π)
-    bigev, ueta1, deta1, veta1 = decomposition_svdct!(etawt1)
-    if existx | (nunstab == 0)
-       eu[1] = 1
-    else
-        if exist
-            eu[1] = -1
-        end
-    end
+
     if isempty(veta1)
         unique = true
     else
-        unique = norm(veta1- A_mul_Bc(veta, veta) * veta1, 2) < ϵ * n
-    end
-    if unique
-       eu[2] = 1
+        loose = veta1 - A_mul_Bc(veta, veta) * veta1
+        loosesvd = svd!(loose)
+        nloose = sum( abs.(loosesvd.S) .> ϵ * n )
+        unique = (nloose == 0)
     end
 
-    tmat = hcat(I(n - nunstab), -ueta1 * deta1 * Ac_mul_B(veta1, veta) * (deta \ ueta'))
-    G0 =  vcat(tmat * a, hcat(zeros(nunstab, n - nunstab), I(nunstab)))
-    G1 =  vcat(tmat * b, zeros(nunstab, n))
-    G1 = G0 \ G1
+    if unique
+        eu[2] = 1
+    else
+        @info("Indeterminacy. $(nloose) loose endogeneous errors")
+    end
+
+    tmat = hcat(I(n - nunstab), -(ueta * (deta \ veta') * veta1 * A_mul_Bc(deta1, ueta1))')
+    G0 = vcat(tmat * a, hcat(zeros(nunstab, n - nunstab), I(nunstab)))
+    G1 = vcat(tmat * b, zeros(nunstab, n))
+
+    # G0 is always non-singular because by construction there are no zeros on
+    # the diagonal of a(1:n-nunstab,1:n-nunstab), which forms G0's ul corner.
+    G0I = inv(G0)
+    G1 = G0I * G1
     usix = (n - nunstab + 1):n
-    C = G0 \ vcat(tmat * Ac_mul_B(qt, c), (a[usix, usix] .- b[usix, usix]) \ Ac_mul_B(qt2, c))
-    impact = G0 \ vcat(tmat * Ac_mul_B(qt, Ψ), zeros(nunstab, size(Ψ, 2)))
-    G1 = z * A_mul_Bc(G1, z)
-    G1 = real(G1)
+    Busix = b[usix,usix]
+    Ausix = a[usix,usix]
+    C = G0I * vcat(tmat * Ac_mul_B(qt, c), (Ausix - Busix) \ Ac_mul_B(qt2, c))
+    impact = G0I * vcat(tmat * Ac_mul_B(qt, Ψ), zeros(nunstab, size(Ψ, 2)))
+    fmat = Busix \ Ausix
+    fwt = -Busix \ Ac_mul_B(qt2, Ψ)
+    ywt = G0I[:, usix]
+
+    loose = G0I * vcat(etawt1 * (I(neta) - A_mul_Bc(veta, veta)), zeros(nunstab, neta))
+
+    G1 = real(z * A_mul_Bc(G1, z))
     C = real(z * C)
     impact = real(z * impact)
-    return G1, C, impact, qt', a, b, z, eu
+    loose = real(z * loose)
+    ywt = z * ywt
+
+    return G1, C, impact, fmat, fwt, ywt, gev, eu, loose
 end
 
 
-
-function new_divct(F::LinearAlgebra.GeneralizedSchur)
+function new_div(F::LinearAlgebra.GeneralizedSchur)
     a, b = F.S, F.T
     n = size(a, 1)
-    div = 0.001
+    div = 1.01
     for i in 1:n
-        if abs(a[i, i]) > ϵ
-            divhat = real(b[i, i] / a[i, i])
-            if (ϵ < divhat) && (divhat < div)
-                div = 0.5 * divhat
+        if abs(a[i, i]) > 0
+            divhat = abs(b[i, i] / a[i, i])
+            if (1 + ϵ < divhat) && (divhat <= div)
+                div = 0.5 * (1 + divhat)
             end
         end
     end
@@ -157,7 +190,7 @@ function new_divct(F::LinearAlgebra.GeneralizedSchur)
 end
 
 
-function decomposition_svdct!(A)
+function decomposition_svd!(A)
     Asvd = svd!(A)
     bigev = findall(Asvd.S .> ϵ)
     Au = Asvd.U[:, bigev]
